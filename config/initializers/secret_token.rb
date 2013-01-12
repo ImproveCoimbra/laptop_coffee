29b7b932d32bd896a24ad1c17e436b73f5951764
Pedro Gaspar <pedro.gaxpar@gmail.com>
2013-01-12 18:07:43 +0000

    First Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LaptopCoffee::Application.config.secret_token = '0626949757545307c4501dd58042eb3e8aae81484e2be78ab4d007f0bbecc57f9069e580a63b096015f4d13c87e72d9b69f2f7bd6b0ee421000f406cc6b6dd6e'
