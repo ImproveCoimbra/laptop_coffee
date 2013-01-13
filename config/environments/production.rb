4f4f43984d2a8bdaeceae2b841ec2a108a510ce8
Pedro Gaspar <pedro.gaxpar@gmail.com>
2013-01-13 19:39:38 +0000

    Removing email, adding some temporary messages and trying to fix precompile errors.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ LaptopCoffee::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Needed for the ActiveAdmin's manifest assets.
+  config.assets.precompile += ['active_admin.css', 'active_admin.js']
 end
