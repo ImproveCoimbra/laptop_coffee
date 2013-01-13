d8a1c7c06ecb971383759365ef174c96cc10afba
Pedro Gaspar <pedro.gaxpar@gmail.com>
2013-01-13 19:18:37 +0000

    Encoding issues.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 ActiveAdmin.setup do |config|
 
   # == Site Title
@@ -5,7 +7,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Laptops - Coffees - Admin"
+  config.site_title = "Laptops ♥ Coffees ♥ Admin"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
