8b82f70c7056d57a41e18fc8aa06c44c6959b278
Pedro Gaspar <pedro.gaxpar@gmail.com>
2013-01-16 13:36:21 +0000

    Displaying sidebar links.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -4,12 +4,13 @@ class FrontController < ApplicationController
   # GET /
   def index
     @json = Place.where(:visible => true).to_gmaps4rails do |place, marker|
-      marker.json({ :id => place.name, :foo => place.address })
+      #marker.json({ :id => place.name, :foo => place.address })
       #marker.picture({
       #  :picture => "http://bundlr.com/images/favicon.png",
       #  :width   => 32,
       #  :height  => 32
       # })
+      marker.sidebar "<span class=\"foo\">#{place.name}</span>"
       marker.title place.name
     end
   end
