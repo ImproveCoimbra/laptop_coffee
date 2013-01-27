89ebd3ed60232cb8880d5379c69a3f0040700727
Pedro Gaspar <pedro.gaxpar@gmail.com>
2013-01-27 23:47:18 +0000

    Functional sidebar.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,7 @@ jQuery(function ($) {
 
     jQueryCache = {
         'window' : $(window),
-        '.overallMap, .gmaps4rails_map' : $('.overallMap, .gmaps4rails_map'),
+        '.overallMap, .gmaps4rails_map, .list-wrapper' : $('.overallMap, .gmaps4rails_map, .list-wrapper'),
         'header' : $('header'),
         'featuresContainer' : $('.featuresContainer')
     };
@@ -28,15 +28,10 @@ jQuery(function ($) {
 
         height = windowHeight - headerHeight - featuresHeight;
 
-        jQueryCache['.overallMap, .gmaps4rails_map'].css('height', height + 'px');
+        jQueryCache['.overallMap, .gmaps4rails_map, .list-wrapper'].css('height', height + 'px');
     };
 
     ajdustSize();
-
-    //On the first time re adjust it to compensate for the eventual size change
-    if (Gmaps && Gmaps.map) {
-        Gmaps.map.adjustMapToBounds();
-    }
 
     //Probably we should throtle this
     jQueryCache.window.bind('resize', ajdustSize);
@@ -58,4 +53,49 @@ jQuery(function ($) {
            shadow_height: null,
         });
     };
+
+  // Map Load callback
+  Gmaps.map.callback = function() {
+
+    // Close open infoboxes on map click
+    google.maps.event.addListener(Gmaps.map.serviceObject, "click", function() {
+      if (Gmaps.map.visibleInfoWindow != null) { Gmaps.map.visibleInfoWindow.close(); }
+    });
+
+    // On the first time re-adjust it to compensate for the eventual size change
+    Gmaps.map.adjustMapToBounds();
+
+    for (var i = 0; i <  this.markers.length; ++i) {
+      google.maps.event.addListener(Gmaps.map.markers[i].serviceObject, 'mouseover', function() {
+        this.setIcon('http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png');
+        //if (this.getAnimation() == null)
+        //  this.setAnimation(google.maps.Animation.BOUNCE);
+      });
+
+      google.maps.event.addListener(Gmaps.map.markers[i].serviceObject, 'mouseout', function() {
+        this.setIcon('http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png');
+        //this.setAnimation(null);
+      });
+
+      //google.maps.event.addListener(Gmaps.map.markers[i].serviceObject, 'click', function() {
+      //  var i = this.__gm_id - 1;
+      //  $($("#markers-list li")[i]).addClass("active");
+      //});
+    }
+  };
+
+  $("#markers-list li").live("mouseover", function(){
+    var i = $(this).index();
+    var marker = Gmaps.map.markers[i].serviceObject;
+    //if (marker.getAnimation() == null)
+      //marker.setAnimation(google.maps.Animation.BOUNCE);
+    marker.setIcon('http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png');
+  });
+
+  $("#markers-list li").live("mouseout", function(){
+    var i = $(this).index();
+    var marker = Gmaps.map.markers[i].serviceObject;
+    //marker.setAnimation(null);
+    marker.setIcon('http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png');
+  });
 });
--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -3,14 +3,18 @@ class FrontController < ApplicationController
 
   # GET /
   def index
-    @json = Place.where(:visible => true).to_gmaps4rails do |place, marker|
+    @json = Place.where(:visible => true).order(:name).to_gmaps4rails do |place, marker|
       #marker.json({ :id => place.name, :foo => place.address })
-      #marker.picture({
-      #  :picture => "http://bundlr.com/images/favicon.png",
-      #  :width   => 32,
-      #  :height  => 32
-      # })
-      marker.sidebar "<span class=\"foo\">#{place.name}</span>"
+      marker.picture({
+        :picture => "http://www.google.com/intl/en_us/mapfiles/ms/micons/red-dot.png",
+        :width   => 32,
+        :height  => 32,
+        #:shadow_picture => "http://www.google.com/intl/en_us/mapfiles/ms/micons/msmarker.shadow.png",
+        #:shadow_height => '32',
+        #:shadow_width => '40',
+        #:shadow_anchor => ['-26px', '-32px'],
+      })
+      marker.sidebar "<i class=\"icon-chevron-right\"></i> #{place.name}"
       marker.title place.name
     end
   end
