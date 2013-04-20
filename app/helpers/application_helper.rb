0a1f36f366fbf7c5e5e9ded4ea4c1d1093283595
Pedro Gaspar <pedro.gaxpar@gmail.com>
2013-04-20 18:23:48 +0100

    First step of the redesign. (Improve Cbr #4 - Sábado)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     URI.join(root_url, image_path(source))
   end
 
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    html = content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+
+    html += content_tag(:li, :class => "divider-vertical") {}
+  end
+
 end
