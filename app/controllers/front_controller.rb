a912bdd29c4b9a659ee2921ce3368cf9a20fd922
Pedro Gaspar <pedro.gaxpar@gmail.com>
2014-01-17 11:49:00 +0000

    Place no longer ruled by its address. Can change coords.

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -20,6 +20,3 @@ class FrontController < ApplicationController
   end
 
 end
-
-
-
--- app/models/place.rb
+++ app/models/place.rb
@@ -3,13 +3,7 @@ class Place < ActiveRecord::Base
   after_validation :process_photo_urls
 
   validates :info_url, :format => { :with => /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?\z/ix, :allow_blank => true }
-  validates :name, :address, :presence => true
-
-  # Ruby Geocoder
-  #geocoded_by :address
-  #after_validation :geocode, :if => lambda{ |obj| obj.address_changed? }
-  reverse_geocoded_by :latitude, :longitude, :address => :location
-  after_validation :reverse_geocode, :if => lambda{ |obj| obj.address_changed? }
+  validates :name, :latitude, :longitude, :presence => true
 
   # Gmaps4Rails
   acts_as_gmappable :process_geocoding => false
